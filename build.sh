fde9ac469924f220c80366343009b08137638272
Carlos Rodrigues <kikito.rodrigues@gmail.com>
2014-12-19 00:17:46 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
 
 echo Compiling SAMPHP plugin... Please stand by.
-g++ -shared -O2 -m32 -o ./samphp  -I/usr/local/include/php -I/usr/local/include/php/Zend -I/usr/local/include/php/TSRM -I/usr/local/include/php/main -I/usr/local/include/php/sapi/embed -I./src -w ./src/*.cpp -w ./src/sampgdk/*.cpp -lrt -lphp5 -lresolv
+g++ -shared -O2 -m32 -o ./samphp  -I/usr/local/include/php -I/usr/local/include/php/Zend -I/usr/local/include/php/TSRM -I/usr/local/include/php/main -I/usr/local/include/php/sapi/embed -I./src -w ./src/*.cpp -w ./src/sampgdk/*.cpp -lrt -lphp5 -lresolv -D SAMPGDK_AMALGAMATION -D __LINUX__
 echo Done.
