6953fe167f4ba7c23a23180a272a17a5a190dfab
Carlos Rodrigues <kikito.rodrigues@gmail.com>
2014-12-21 14:18:13 +0000

    Updated the buid command
    Fixed issues compiling on linux.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -e
 
 echo Compiling SAMPHP plugin... Please stand by.
-g++ -shared -O2 -m32 -o ./samphp  -I/usr/local/include/php -I/usr/local/include/php/Zend -I/usr/local/include/php/TSRM -I/usr/local/include/php/main -I/usr/local/include/php/sapi/embed -I./src -w ./src/*.cpp -w ./src/sampgdk/*.c -lrt -lphp5 -lresolv -D SAMPGDK_AMALGAMATION -D __LINUX__
-echo Done.
+gcc -c -o ./sampgdk.o ./src/sampgdk/sampgdk.c -D SAMPGDK_AMALGAMATION -D linux
+g++ -shared -O2 -m32 -o ./samphp sampgdk.o  -I/usr/local/include/php -I/usr/local/include/php/Zend -I/usr/local/include/php/TSRM -I/usr/local/include/php/main -I/usr/local/include/php/sapi/embed -I./src -w ./src/*.cpp -w ./src/sampgdk/*.cpp -lrt -lphp5 -lresolv -D __LINUX__
+echo Done
